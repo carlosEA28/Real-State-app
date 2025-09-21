6ac441cfcb90a5addc42c945673730ca15ef6117
carlosEA28 <carloseaduarte28@gmail.com>
2025-09-21 17:43:22 -0300

    feat: add auth screen without logic

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,22 +4,24 @@ import { useEffect } from "react";
 import "./globals.css";
 
 export default function RootLayout() {
-  const [fontLoaded] = useFonts({
-    "Rubik-Bold": "../assets/fonts/Rubik_Bold.ttf",
-    "Rubik-ExtraBold": "../assets/fonts/Rubik_ExtraBold.ttf",
-    "Rubik-Light": "../assets/fonts/Rubik_Light.ttf",
-    "Rubik-Medium": "../assets/fonts/Rubik_Medium.ttf",
-    "Rubik-Regular": "../assets/fonts/Rubik_Regular.ttf",
-    "Rubik-SemiBold": "../assets/fonts/Rubik_SemiBold.ttf",
+  const [fontsLoaded] = useFonts({
+    "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+    "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
+    "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
   });
 
   useEffect(() => {
-    if (fontLoaded) {
+    if (fontsLoaded) {
       SplashScreen.hideAsync();
     }
-  }, [fontLoaded]);
+  }, [fontsLoaded]);
 
-  if (!fontLoaded) {
-    return <Stack />;
+  if (!fontsLoaded) {
+    return null; // mantém splash enquanto fontes carregam
   }
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,9 +13,10 @@ module.exports = {
       fontFamily: {
         rubik: ["Rubik-Regular", "sans-serif"],
         "rubik-bold": ["Rubik-Bold", "sans-serif"],
-        "rubik-extrabold": ["Rubik-Regular", "sans-serif"],
-        "rubik-light": ["Rubik-Light", "sans-serif"],
+        "rubik-extrabold": ["Rubik-ExtraBold", "sans-serif"],
         "rubik-medium": ["Rubik-Medium", "sans-serif"],
+        "rubik-semibold": ["Rubik-SemiBold", "sans-serif"],
+        "rubik-light": ["Rubik-Light", "sans-serif"],
       },
       colors: {
         primary: {
