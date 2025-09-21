ed5734b54a44cb84481a3af6ed227885a8f0fec0
carlosEA28 <carloseaduarte28@gmail.com>
2025-09-21 17:19:48 -0300

    feat: add inital splash screen setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,25 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 import "./globals.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontLoaded] = useFonts({
+    "Rubik-Bold": "../assets/fonts/Rubik_Bold.ttf",
+    "Rubik-ExtraBold": "../assets/fonts/Rubik_ExtraBold.ttf",
+    "Rubik-Light": "../assets/fonts/Rubik_Light.ttf",
+    "Rubik-Medium": "../assets/fonts/Rubik_Medium.ttf",
+    "Rubik-Regular": "../assets/fonts/Rubik_Regular.ttf",
+    "Rubik-SemiBold": "../assets/fonts/Rubik_SemiBold.ttf",
+  });
+
+  useEffect(() => {
+    if (fontLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontLoaded]);
+
+  if (!fontLoaded) {
+    return <Stack />;
+  }
 }
