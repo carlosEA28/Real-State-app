e71358cf77d45123736cb245b451d6746e4169fd
carlosEA28 <carloseaduarte28@gmail.com>
2025-09-21 16:42:01 -0300

    feat: add nativewind setup

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from "expo-router";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -10,6 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text className="font-bold bg-red-500">Welcome to ReState</Text>
       <Link href={"/sign-in"}>Sign in</Link>
       <Link href={"/explore"}>Explore</Link>
       <Link href={"/profile"}>Profile</Link>
